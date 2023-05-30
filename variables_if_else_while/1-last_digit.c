61fbf3633237c92b9536039ee6b44f5516562cea
root <coltonisbeast1231@gmail.com>
2023-05-30 12:48:10 -0700

    dff

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: is 0
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+int x = n % 10 
+ if (x > 5)
+else if (x == 0)
+ else if((x < 6) && (x > 0)
+
+
+return (0);
+}
