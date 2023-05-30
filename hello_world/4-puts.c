37c525d6dc9690c6530f10980cd25d87ff2fcf2f
root <coltonisbeast1231@gmail.com>
2023-05-30 08:37:15 -0700

    cdf

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * return: return 0 on success
+ */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle"");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
